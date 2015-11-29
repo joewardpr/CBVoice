694327a3377086b7f9c560dc119b6209abaf38b8
Joe Ward <joewardpr@gmail.com>
2015-11-29 14:20:53 -0400

    Save feedback to database for Politico controller
    Note: Politico, not to be confused with the plural version controller
    Politicos (for the list).

--- www/js/controllers/politico.js
+++ www/js/controllers/politico.js
@@ -1,11 +1,26 @@
-app.controller('PoliticoCtrl', function($scope, $stateParams, $state, Auth, $location, FURL, $firebaseObject) {
+app.controller('PoliticoCtrl',
+  function($scope, $stateParams, $state, Auth, $location, FURL, $firebaseArray, $firebaseObject ) {
 
   // Bind Firebase list of elected officials to the list
-  ref = new Firebase(FURL);
+  $scope.ref = new Firebase(FURL)
+  $scope.msgRef = new Firebase(FURL + "/feedback")
+  $scope.feedback = $firebaseArray($scope.msgRef)
+
+
   userId = $stateParams.userId
-  $scope.politico = $firebaseObject(ref.child('politicos/' + userId));
+  $scope.politico = $firebaseObject($scope.ref.child('politicos/' + userId))
 
   console.log($stateParams.userId)
+
+  $scope.sendFeedback = function() {
+    console.log("Saving message into database for moderation.")
+
+    $scope.feedback.$add({
+      msg: $scope.feedbackMsg,
+      approved: 'false'
+    });
+
+  }
 
 })
 
