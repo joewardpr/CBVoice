0212f85202cb8cf1430faea785cf0f6d94749a95
Joe Ward <joewardpr@gmail.com>
2015-11-29 15:47:51 -0400

    Success of fail messages with animation

--- www/js/controllers/politico.js
+++ www/js/controllers/politico.js
@@ -1,11 +1,12 @@
 app.controller('PoliticoCtrl',
-  function($scope, $stateParams, $state, Auth, $location, FURL, $firebaseArray, $firebaseObject ) {
+  function($scope, $stateParams, $state, Auth, $location, FURL, $firebaseArray, $firebaseObject, $templateCache ) {
+
+  $scope.pageloaded = "{'display : none'}";
 
   // Bind Firebase list of elected officials to the list
   $scope.ref = new Firebase(FURL)
   $scope.msgRef = new Firebase(FURL + "/feedback")
   $scope.feedback = $firebaseArray($scope.msgRef)
-
 
   userId = $stateParams.userId
   $scope.politico = $firebaseObject($scope.ref.child('politicos/' + userId))
@@ -28,7 +29,6 @@ app.controller('PoliticoCtrl',
       $scope.feedbackMsg = '' // Reset
 
       $scope.submitted = true
-
 
     }
     else {
